c65f8278ddba216b5218039a6f56ed997d270ce3
Mercedes Ubeira Romero <mercedesubeira@gmail.com>
2025-05-19 20:51:10 +0200

    Revert "Update vite.config.js"
    This reverts commit 90a210450687dc766176e0010182373acb748e20.

--- thesis-app/vite.config.js
+++ thesis-app/vite.config.js
@@ -1,5 +1,7 @@
-export default {
-  build: {
-    outDir: 'dist'
-  }
-};
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
